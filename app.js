0870fa5ee1abe41bbb39039deb444155e448cb00
Anupam <anupamdebnath6335@gmail.com>
2022-11-20 16:04:04 +0530

    error fixed

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
-// Define UI variable
+
+// Define UI Vars
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('.task');
+const taskInput = document.querySelector('#task');
 
+// Load all event listeners
+loadEventListeners();
 
-// load all even listenser
-loadAllEventListners();
-
-function loadAllEventListners() {
-  // add task event
+// Load all event listeners
+function loadEventListeners() {
+  // Add task event
   form.addEventListener('submit', addTask);
 }
 
-// Add Task function
+// Add Task
 function addTask(e) {
-
   if(taskInput.value === '') {
     alert('Add a task');
   }
 
-  // create element
-  const li = document.querySelector('li');
-  // li class name
+  // Create li element
+  const li = document.createElement('li');
+  // Add class
   li.className = 'collection-item';
-  // create Text node appened to the li
+  // Create text node and append to li
   li.appendChild(document.createTextNode(taskInput.value));
-  // create new link
-  const link = document.createElement(a);
+  // Create new link element
+  const link = document.createElement('a');
   // Add class
   link.className = 'delete-item secondary-content';
-  // Add icon
+  // Add icon html
   link.innerHTML = '<i class="fa fa-remove"></i>';
-  // append link to li
+  // Append the link to li
   li.appendChild(link);
-  // console.log(li);
-  // append li to ul
-  taskList.appendChild(li);  
 
-  // clear task 
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Clear input
   taskInput.value = '';
+
   e.preventDefault();
 }
