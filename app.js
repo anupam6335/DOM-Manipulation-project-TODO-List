b1d262f93a1ed1a24ac35a33754d545e40351be8
Anupam <anupamdebnath6335@gmail.com>
2022-11-24 12:07:24 +0530

    final part is left

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // remove task event
+  taskList.addEventListener('click', removeTask);
+  // clear task event
+  clearBtn.addEventListener('click', clearTask);
+  // filter task event
+  filter.addEventListener('keyup', filterTask);
 }
 
 // Add Task
@@ -39,8 +45,60 @@ function addTask(e) {
   // Append li to ul
   taskList.appendChild(li);
 
+  // store in local storage
+  storeTaskInLocalStorage(taskInput.value);
+
   // Clear input
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+// local storage
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } 
+  else{
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  } 
+
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+
+// remove task
+function removeTask (e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are you sure you want to delete'))
+      e.target.parentElement.parentElement.remove();
+  }
+  // console.log(e.target);
+}
+
+// clear task 
+function clearTask (e) {
+  // taskList.innerHTML = '';
+
+  // Faster way to remove
+  if(confirm('Are you sure you want to delete this whole task?'))
+    while(taskList.firstChild) {
+      taskList.removeChild(taskList.firstChild);
+   }
+}
+
+
+function filterTask(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    }else {
+      task.style.display = 'none';
+    }
+  });
 }
