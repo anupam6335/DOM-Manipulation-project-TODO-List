24fec677c9cd51c872f00c8c3d37e5a054af2493
Anupam <anupamdebnath6335@gmail.com>
2022-11-25 14:25:11 +0530

    complete emi calculator

--- Loan Calculator/app.js
+++ Loan Calculator/app.js
@@ -1,6 +1,17 @@
 
 
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  
+    // Show results
+    document.getElementById('results').style.display = 'none';
+
+    // Hide loader
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault();
+});
 
 function calculateResults(e) {
     console.log('Calculating...');
@@ -30,8 +41,40 @@ function calculateResults(e) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayment).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayment) - principal).toFixed(2);
+        
+        
+        // Show results
+        document.getElementById('results').style.display = 'block';
+
+        // Hide loader
+        document.getElementById('loading').style.display = 'none';
     }else {
-        console.log('Please Check your payment');
+       showError();
     }
-    e.preventDefault();
+
+}
+
+function showError(error='please check your amount') {
+    // create Div
+    const errorDiv = document.createElement("div");
+
+    // get elemenents
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // add class to the div 
+    errorDiv.className = "alert alert-danger";
+    
+    // create a text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // insert errer above heading
+    card.insertBefore(errorDiv, heading);
+
+    // clear error after 3 seconds
+    setTimeout(clearError, 3000);
+}
+
+function clearError() {
+    document.querySelector('.alert').remove();
 }
